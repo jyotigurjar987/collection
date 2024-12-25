cbcfac239fe401b51d90db20299d0e67feca14fc
jyotigurjar987 <jyansh987@gmail.com>
2024-12-26 00:06:06 +0530

    collection file

---
+++ CollectionDemo1.java
@@ -0,0 +1,19 @@
+package oops;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+public class CollectionDemo1 {
+	
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		list.add("Hari");
+		list.add("Ravi");
+		list.add("Kavi");
+		Iterator<String> itr=list.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+	}
+
+}
